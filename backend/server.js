473976e63ad24c5d42929676b3ea2a54b04a5286
hyunho4532 <kyounguk192@gmail.com>
2024-05-06 22:02:17 +0900

    2024-05-06 헬핑! 기능 구현 중(채팅방 UI 화면 구현 완료 && DynamoDB Insert 완료)

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,11 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
+aws.config.update ({
+    region: 'ap-northeast-2',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
 
 app.post('/errorTypeData', (req, res) => {
     https.get('https://czwpwf5o4m.execute-api.ap-northeast-2.amazonaws.com/stage/error/write', (response) => {
@@ -41,12 +46,6 @@ app.post('/platformData', (req, res) => {
 
 app.post('/errorBoardData', (req, res) => {
 
-    aws.config.update ({
-        region: 'ap-northeast-2',
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    })
-
     const body = req.body;
 
     console.log(body);
@@ -69,12 +68,6 @@ app.post('/errorBoardData', (req, res) => {
 
 app.post('/userData', (req, res) => {
 
-    aws.config.update ({
-        region: 'ap-northeast-2',
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    })
-
     const body = req.body;
 
     console.log(body);
@@ -95,6 +88,25 @@ app.post('/userData', (req, res) => {
     });
 })
 
+app.post('/errorHelpingData', (req, res) => {
+    const body = req.body;
+
+    const params = {
+        TableName: 'HELPING',
+        Item: body
+    }
+
+    docClient.put(params, (err, data) => {
+        if (err) {
+            console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+            res.status(500).send('Error saving data to DynamoDB');
+        } else {
+            console.log('Added Item:', JSON.stringify(data, null, 2));
+            res.json(body);
+        }
+    })
+})
+
 app.get('/detail', (req, res) => {
     const author = req.query.author;
     const type = req.query.type;
@@ -103,12 +115,6 @@ app.get('/detail', (req, res) => {
 });
 
 app.get('/errorBoardData/get/web', (req, res) => {
-    
-    aws.config.update ({
-        region: 'ap-northeast-2',
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    })
 
     const params = {
         TableName: 'errorBoard',
@@ -125,11 +131,6 @@ app.get('/errorBoardData/get/web', (req, res) => {
 });
 
 app.get('/errorFind/avg/android', (req, res) => {
-    aws.config.update ({
-        region: 'ap-northeast-2',
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    })
 
     const params = {
         TableName: 'errorBoard'
@@ -146,11 +147,6 @@ app.get('/errorFind/avg/android', (req, res) => {
 })
 
 app.get('/errorFind/avg/devops', (req, res) => {
-    aws.config.update ({
-        region: 'ap-northeast-2',
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    })
 
     const params = {
         TableName: 'errorBoard'
@@ -167,11 +163,6 @@ app.get('/errorFind/avg/devops', (req, res) => {
 })
 
 app.get('/errorFind/avg/web', (req, res) => {
-    aws.config.update ({
-        region: 'ap-northeast-2',
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    })
 
     const params = {
         TableName: 'errorBoard',
@@ -189,12 +180,6 @@ app.get('/errorFind/avg/web', (req, res) => {
 })
 
 app.get('/errorBoardData/get', (req, res) => {
-    
-    aws.config.update ({
-        region: 'ap-northeast-2',
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    })
 
     const params = {
         TableName: 'errorBoard',
--- frontend/src/view/heping/insert/HelpingInsert.tsx
+++ frontend/src/view/heping/insert/HelpingInsert.tsx
@@ -1,6 +1,54 @@
+import { useEffect, useState } from 'react';
 import './HelpingInsert.scss'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 function HelpingInsert() {
+
+    const [errorNameData, setErrorNameData] = useState(null);
+    const [errorSituationData, setErrorSituationData] = useState(null);
+    const [errorRequestData, setErrorRequestData] = useState(null);
+
+    const userAuthUid = localStorage.getItem('authuid')
+    const userAuthUidFromJson = JSON.parse(userAuthUid!);
+
+    useEffect(() => {
+        console.log(userAuthUidFromJson.authuid)
+    }, [userAuthUidFromJson.authuid])
+
+    const handleErrorNameChange = (e: any) => {
+        setErrorNameData(e.target.value);
+    }
+
+    const handleErrorSituationChange = (e: any) => {
+        setErrorSituationData(e.target.value);
+    }
+
+    const handleErrorRequestChange = (e: any) => {
+        setErrorRequestData(e.target.value);
+    }
+
+    const notify = () => {
+
+        const data = {
+            id: userAuthUidFromJson.authuid,
+            errorName: errorNameData,
+            errorSituation: errorSituationData,
+            errorRequest: errorRequestData
+        }
+    
+        axios.post('http://localhost:50000/errorHelpingData', data)
+            .then(response => {
+                console.log("응답 받음", response.data);
+            })
+            .catch(error => {
+                console.log("응답 중 에러가 발생함", error);
+            })
+
+        toast.success("헬핑이 등록되었어요!!");
+    }
+
     return (
         <div className="helping-insert">
             <h2 className="helping-insert-title">헬핑 등록하기</h2>
@@ -8,19 +56,24 @@ function HelpingInsert() {
             <h5 className="helping-insert-title">* 에러를 등록하고, 다른 사람들이 댓글로 에러를 분석해도 안될 때 이용해주세요.</h5>
             <div className="helping-insert-component">
                 <h3>1. 에러가 어디서 발생하셨나요? 💣</h3>
-                <select className="helping-insert-select">
+                <select className="helping-insert-select" onChange={handleErrorNameChange}>
                     <option>안드로이드</option>
                     <option>DevOps</option>
                     <option>웹</option>
                 </select>
 
-                <h3>2. 에러가 발생했을 때 상황을 입력해주세요 💣</h3>
-                <select className="helping-insert-select">
-                    <option>안드로이드</option>
-                    <option>DevOps</option>
-                    <option>웹</option>
-                </select>
+                <h3>2. 에러가 발생했을 때 상황을 입력해주세요 👓</h3>
+                <input className="helping-insert-input" placeholder="ex) 앱 실행 시 튕기는 현상 발생" onChange={handleErrorSituationChange}></input>
+
+                <h3>3. 요청 사항💣</h3>
+                <input className="helping-insert-input" placeholder="ex) 에러 분석 잘하는 개발자만 와주세요 ㅠㅠ" onChange={handleErrorRequestChange}></input>
             </div>
+
+            <button className="helping-insert-button" onClick={notify}>
+                헬핑! 등록하기
+            </button>
+
+            <ToastContainer />
         </div>
     )
 }
