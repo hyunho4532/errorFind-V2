6ddab419c2c0a3e250afd519ee31fa1ba5edb3cf
hyunho4532 <kyounguk192@gmail.com>
2024-05-14 18:59:04 +0900

    프로필 상세 기능 구현 중

--- frontend/src/view/errorfind/profile/ErrorFindProfile.tsx
+++ frontend/src/view/errorfind/profile/ErrorFindProfile.tsx
@@ -14,6 +14,11 @@ function ErrorFindProfile() {
 
     return (
         <div className={themeIsNight ? 'dark-mode' : ''}>
+            
+            <div style={{ width: '120px', height: '120px', borderRadius: '70%', overflow: 'hidden', border: '1.5px solid black' }}>
+                <img src="../../../public/guest_logo.png" alt="rounded_image" style={{ width: '100%', height: '100%', display: 'block' }} />
+            </div>
+
             <Card style={{ width: "600px", height: "60px", display: "flex", justifyContent: "space-between" }}>
                 <p style={{ textAlign: "start", paddingLeft: "16px", fontWeight: "bold" }}>다크 모드 활성화</p>
                 
@@ -23,7 +28,13 @@ function ErrorFindProfile() {
                 
             </Card>
 
-            
+            <Card style={{ width: "600px", height: "60px", display: "flex", justifyContent: "space-between", marginTop: "32px" }}>
+                <p style={{ textAlign: "start", paddingLeft: "16px", fontWeight: "bold" }}>계정 탈퇴</p>
+
+                <div style={{ alignContent: "center", marginRight: "8px" }}>
+                    <button style={{ backgroundColor: "#F05650", fontSize: "16px", color: "white", fontWeight: "bold" }}>계정 탈퇴 진행하기</button>
+                </div>
+            </Card>
         </div>
     )
 }
