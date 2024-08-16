3ee30a88d6f379ab189bd4c5388f956f3e3fdca0
hyunho4532 <kyounguk192@gmail.com>
2024-08-16 14:34:32 +0900

    Question Page Working

---
+++ frontend/src/config.ts
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        key: 1,
+        title: "자바스크립트",
+        icon: "✌️",
+        link: "/question/javascript"
+    },
+    {
+        key: 2,
+        title: "타입스크립트",
+        icon: "🤩",
+        link: "/question/typescript"
+    },
+    {
+        key: 3,
+        title: "React",
+        icon: "😊",
+        link: "/question/react"
+    },
+    {
+        key: 4,
+        title: "Next",
+        icon: "😍",
+        link: "/question/next"
+    },
+    {
+        key: 5,
+        title: "Spring",
+        icon: "👌",
+        link: "/question/spring"
+    }
+]
--- frontend/src/view/question/Question.tsx
+++ frontend/src/view/question/Question.tsx
@@ -1,11 +1,27 @@
+import { useRecoilValue } from "recoil";
 import { SideBar } from "../../component/sidebar";
 import './index.scss';
+import { state } from "../../recoil/Atom";
 
 export function Question() {
+
+    const link = useRecoilValue(state);
+
+    console.log(link);
+
     return (
         <div className="container">
             <SideBar />
             <div className="main-content">
+                { link === '/question/javascript' 
+                    ? <p>자바스크립트 질문</p> 
+                    : link === '/question/typescript'
+                        ? <p>타입스크립트 질문</p> 
+                    : link === '/question/react'
+                        ? <p>리액트 질문.</p>
+                    : link === '/question/next'
+                        ? <p>Next 질문.</p>
+                    : <p>Spring 질문.</p> }
             </div>
         </div>
     );
